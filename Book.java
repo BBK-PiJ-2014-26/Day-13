389842bd8f1e322252aba3c729765585d24180e8
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-01-12 18:38:07 +0000

    First commit.

---
+++ Book.java
@@ -0,0 +1,13 @@
+public interface Book {
+
+/**
+*  @return a string called authorName.
+*/
+public String getAuthorName();
+
+/**
+* @return a string called title.
+*/
+public String getTitle();
+
+}
