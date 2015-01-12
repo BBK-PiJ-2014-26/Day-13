a5076c5768175437040c6d90932cc5e45268514e
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-01-12 19:37:45 +0000

    First commit.

---
+++ User.java
@@ -0,0 +1,17 @@
+public interface User {
+
+/**
+* @return A String representing the User's name.
+*/
+public String getName();
+
+/**
+* @return An int represting the unique userID.
+*/
+public int getUserID();
+
+/**
+* Sets a as User's unique userID.
+*/
+public void setUserID();
+}
